e2710142cd51bd55545fcb40a875ec6047d3f498
GustavoMedeiros-A <gustavomedeiros15@outlook.com.br>
2025-02-08 15:54:31 -0300

    feat: start frontend

---
+++ nextjs-frontend/src/app/page1/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+
+function Page1() {
+    return (
+        <div>
+            <h1>Page1</h1>
+            <Button variant='contained' color='primary'>This is button</Button>
+        </div>
+    )
+}
+
+export default Page1;
